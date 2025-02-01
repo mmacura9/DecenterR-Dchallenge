c88211041bee7298e23c2890b4d46e99497d0bd9
mmacura9 <marko.macura9@gmail.com>
2025-02-01 11:48:12 +0100

    Remove comments

--- src/utils/web3.ts
+++ src/utils/web3.ts
@@ -5,8 +5,7 @@ const generateWeb3 = () => {
         alert('Please install MetaMask first.');
         throw new Error('MetaMask not found');
     }
-    // const INFURA_URL = `https://mainnet.infura.io/v3/8b33380e04f34efa9ed482be7c9aa80b`;
-    // console.log(INFURA_URL);
+    
     const web3 = new Web3(window.ethereum);
     return web3;
 }
